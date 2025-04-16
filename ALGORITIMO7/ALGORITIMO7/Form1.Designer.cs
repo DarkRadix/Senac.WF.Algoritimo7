29e283f75d7c8d7c8a6479dbbec938a5df53c36a
Yuri <radixfire2@gmail.com>
2025-04-15 21:33:17 -0300

    inserimos botoes no projeto

--- ALGORITIMO7/ALGORITIMO7/Form1.Designer.cs
+++ ALGORITIMO7/ALGORITIMO7/Form1.Designer.cs
@@ -28,12 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            checkBox1 = new CheckBox();
+            label1 = new Label();
+            label2 = new Label();
+            linkLabel1 = new LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Cursor = Cursors.Hand;
+            pictureBox2.Image = Properties.Resources.ChatGPT_Image_10_de_abr__de_2025__20_19_44;
+            pictureBox2.Location = new Point(911, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(38, 35);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += checkBox1_CheckedChanged;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Cursor = Cursors.Hand;
+            pictureBox3.Image = Properties.Resources.ChatGPT_Image_10_de_abr__de_2025__20_07_09__1_;
+            pictureBox3.Location = new Point(955, 0);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(38, 35);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Cursor = Cursors.IBeam;
+            textBox1.Location = new Point(459, 109);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(211, 24);
+            textBox1.TabIndex = 3;
+            textBox1.TabIndexChanged += textBox1_TextChanged;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Cursor = Cursors.IBeam;
+            textBox2.Location = new Point(459, 171);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(211, 24);
+            textBox2.TabIndex = 4;
+            textBox2.TabIndexChanged += textBox2_TextChanged;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.BackgroundImageLayout = ImageLayout.Zoom;
+            button1.Cursor = Cursors.Hand;
+            button1.FlatAppearance.BorderColor = Color.Black;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Black", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(439, 266);
+            button1.Name = "button1";
+            button1.Size = new Size(293, 62);
+            button1.TabIndex = 5;
+            button1.Text = "Entrar";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.BackColor = Color.Transparent;
+            checkBox1.Cursor = Cursors.Hand;
+            checkBox1.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkBox1.Location = new Point(468, 208);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(170, 29);
+            checkBox1.TabIndex = 6;
+            checkBox1.Text = "Lembrar Senha";
+            checkBox1.UseVisualStyleBackColor = false;
+            checkBox1.CheckedChanged += textBox2_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(458, 81);
+            label1.Name = "label1";
+            label1.Size = new Size(80, 25);
+            label1.TabIndex = 7;
+            label1.Text = "Usuario";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(458, 143);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 25);
+            label2.TabIndex = 8;
+            label2.Text = "Senha";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.BackColor = Color.Transparent;
+            linkLabel1.Cursor = Cursors.Hand;
+            linkLabel1.DisabledLinkColor = Color.DimGray;
+            linkLabel1.LinkColor = Color.FromArgb(192, 0, 0);
+            linkLabel1.Location = new Point(519, 143);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(151, 25);
+            linkLabel1.TabIndex = 9;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Recuperar Acesso";
+            linkLabel1.VisitedLinkColor = Color.FromArgb(192, 0, 0);
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            linkLabel1.Click += textBox2_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            BackgroundImage = Properties.Resources.adc581cdf930efa7b1818c640c2a73ee;
+            BackgroundImageLayout = ImageLayout.Zoom;
+            ClientSize = new Size(993, 559);
+            Controls.Add(linkLabel1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(checkBox1);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private CheckBox checkBox1;
+        private Label label1;
+        private Label label2;
+        private LinkLabel linkLabel1;
     }
 }
--- ALGORITIMO7/ALGORITIMO7/Form1.cs
+++ ALGORITIMO7/ALGORITIMO7/Form1.cs
@@ -1,10 +1,99 @@
+using System.Diagnostics;
+
 namespace ALGORITIMO7
 {
     public partial class Form1 : Form
     {
+        // Variáveis globais
+        private string senhaReal = "";  // Para armazenar a senha real
+        private bool atualizandoTexto = false;  // Evitar loop infinito ao atualizar o texto
+
         public Form1()
         {
             InitializeComponent();
         }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        // Método para fechar o aplicativo
+        private void label1_Click(object sender, EventArgs e)
+        {
+            var resposta = MessageBox.Show("Você deseja fechar está aplicação ?",
+                "Sair",
+                MessageBoxButtons.YesNo
+                , MessageBoxIcon.Question);
+            if (resposta == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        // Evento TextChanged do TextBox para esconder a senha
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            // Evita que o código entre em loop infinito
+            if (atualizandoTexto) return;
+
+            // Marca que estamos atualizando o texto
+            atualizandoTexto = true;
+
+          
+            string textoDigitado = textBox2.Text;
+
+
+            int tamanhoDigitado = textoDigitado.Length;
+            int tamanhoReal = senhaReal.Length;
+
+            if (tamanhoDigitado < tamanhoReal)
+            {
+            
+                senhaReal = senhaReal.Substring(0, tamanhoDigitado);
+            }
+            else if (tamanhoDigitado > tamanhoReal)
+            {
+                
+                char novoChar = textoDigitado[^1];
+                senhaReal += novoChar;
+            }
+
+           
+            if (senhaReal.Length == 0)
+            {
+                textBox2.Text = "";
+            }
+            else if (senhaReal.Length == 1)
+            {
+                textBox2.Text = senhaReal;
+            }
+            else
+            {
+                string visivel = new string('*', senhaReal.Length - 1) + senhaReal[^1];
+                textBox2.Text = visivel;
+            }
+
+
+            textBox2.SelectionStart = textBox2.Text.Length;
+
+            atualizandoTexto = false;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "https://senac.blackboard.com/webapps/blackboard/password",
+                UseShellExecute = true
+            };
+           Process.Start(psi);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Usuario")
+                textBox1.Text = string.Empty;
+        }
     }
 }
+    
